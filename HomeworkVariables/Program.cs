8be54985534e04b198cdccac6bdd75b2c4a0ba12
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-01 02:17:36 -0600

    created person name

---
+++ HomeworkVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Create a Console Application that has variables to hold
+ * a person's name, age, if they are alive, and their phone number. 
+ * You do not need to capture these values from the user.
+ */
+
+// Person's Name
+
+string firstName = "John";
+string lastName = "Doe";
+
+Console.WriteLine( firstName + " " + lastName);
+
+// Person's Age
+// If they are alive or not
+// Person's phone number
